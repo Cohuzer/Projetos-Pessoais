ce61f5fcb3580ff6b5b376a5263a440cfba1b4b0
Mateus <matebatichotti@gmail.com>
2021-04-14 19:21:15 -0300

    Update HP_Boomerang.py

--- Projetos_Pessoais/HP_Boomerang.py
+++ Projetos_Pessoais/HP_Boomerang.py
@@ -1,11 +1,14 @@
-normal = 0
-reverso = -1
-entrada = str(input('Insira a frase a ser boomerangada: '))
-entrada = entrada.replace(' ', '')
-entrada = entrada.upper()
+entrada1 = int(input('Número de frases a serem registradas: '))
+for k in range(0, entrada1):
+    normal = 0
+    reverso = -1
+    entrada = str(input('Insira a frase a ser boomerangada: '))
+    entrada = entrada.replace(' ', '')
+    entrada = entrada.replace('.', '')
+    entrada = entrada.upper()
 
-for i in range(0, len(entrada)):
-    print(f'{entrada[normal]}{entrada[reverso]}')
-    if normal < len(entrada):
-        normal += 1
-        reverso -= 1
+    for i in range(0, len(entrada)):
+        print(f'{entrada[normal]}{entrada[reverso]}')
+        if normal < len(entrada):
+            normal += 1
+            reverso -= 1
