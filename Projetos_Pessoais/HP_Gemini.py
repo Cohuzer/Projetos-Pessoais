bcb1c72213c3dd451eb7a6e9f9ea7b028afcd100
Mateus <matebatichotti@gmail.com>
2021-04-19 13:29:25 -0300

    .

--- Projetos_Pessoais/HP_Gemini.py
+++ Projetos_Pessoais/HP_Gemini.py
@@ -1,5 +1,7 @@
 c = 0
-while c != 11:
+quantiaFrases = int(input('Quantidade de frases serão catalogadas: '))
+quantiaFrases = quantiaFrases
+while c != quantiaFrases:
     entrada = str(input('\nFrase do acróstico: '))
     multiplicacao = int(input('Valor para multiplicar: '))
     x = 0
