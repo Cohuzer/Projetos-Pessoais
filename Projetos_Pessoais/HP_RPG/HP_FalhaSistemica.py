423f5404a44572b083acf1eb2f5dfc2a7dd760cd
Mateus <matebatichotti@gmail.com>
2021-05-26 12:20:53 -0300

    reformat

--- Projetos_Pessoais/HP_RPG/HP_FalhaSistemica.py
+++ Projetos_Pessoais/HP_RPG/HP_FalhaSistemica.py
@@ -1,21 +1,24 @@
 entrada1 = int(input('Número de frases a serem registradas: '))
 
+
 def EscreverFrente(frase):
-        for i in range(0, len(frase)):
-            print(f'{frase[i]}', end='')
+    for i in range(0, len(frase)):
+        print(f'{frase[i]}', end='')
+
 
 def EscreverContrario(frase):
-    for j in range((len(frase)-1), -1, -1):
+    for j in range((len(frase) - 1), -1, -1):
         print(f'{frase[j]}', end='')
 
+
 for k in range(0, entrada1):
-    entrada = str(input(f'\n{k+1}°- Frase: '))
+    entrada = str(input(f'\n{k + 1}°- Frase: '))
     entrada = entrada.upper()
     entrada = entrada.replace('´', '`')
-    entrada = entrada.replace('`','´')
+    entrada = entrada.replace('`', '´')
     entrada = entrada.replace(' ', '')
     copia_entrada = entrada
 
-    for a in range(0, 2*len(entrada)):
+    for a in range(0, 2 * len(entrada)):
         EscreverFrente(entrada)
         EscreverContrario(entrada)
