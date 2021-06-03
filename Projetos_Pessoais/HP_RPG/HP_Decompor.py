0b02eb66686f149fe33f1f3eb27258a886d24d00
Mateus <matebatichotti@gmail.com>
2021-06-03 09:00:53 -0300

    Update HP_Decompor.py

--- Projetos_Pessoais/HP_RPG/HP_Decompor.py
+++ Projetos_Pessoais/HP_RPG/HP_Decompor.py
@@ -1,7 +1,7 @@
 x = int(input('Quantidade de frases a serem catalogadas: '))
 for k in range(x):
     entrada = input('Frase a ser manipulada: ').strip()
-    print(f'{k}°->')
+    print(f'{k}° Frase->')
     for j in range(len(entrada), 0, -1):
         if entrada[j-1] != ' ':
             print(f'{entrada[0:j]}')
