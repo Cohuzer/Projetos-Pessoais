3313eb4c65d0d068e8a661ac8cc30eff240c016f
Mateus <matebatichotti@gmail.com>
2021-06-03 08:47:11 -0300

    Update HP_Reducto.py

--- Projetos_Pessoais/HP_RPG/HP_Reducto.py
+++ Projetos_Pessoais/HP_RPG/HP_Reducto.py
@@ -1,4 +1,3 @@
-lista = []
 entrada = input('Frase a ser manipulada: ')
 entrada = entrada.upper()
 print('Frase-')
@@ -6,4 +5,4 @@ print('Frase-')
 print(entrada)
 for i in range(len(entrada)):
     for j in range(len(entrada), 0, -1):
-        print(f'{entrada[0:j].upper()}')
+        print(f'{entrada[0:j].upper()}{entrada[j:].lower()}')
