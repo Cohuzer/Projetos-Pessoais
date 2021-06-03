20a77ef4f1334c8ecce1d545beaac85a93863cd4
Mateus <matebatichotti@gmail.com>
2021-06-03 09:00:37 -0300

    nHakai

--- Projetos_Pessoais/HP_RPG/HP_Reducto.py
+++ Projetos_Pessoais/HP_RPG/HP_Reducto.py
@@ -1,8 +1,10 @@
-entrada = input('Frase a ser manipulada: ').strip()
-entrada = entrada.upper()
-print('Frase-')
+x = int(input('Quantidade de frases a serem catalogadas: '))
+for i in range(x):
+    entrada = input('Frase a ser manipulada: ').strip()
+    entrada = entrada.upper()
+    print(f'{i+1}° Frase-')
 
-for j in range(len(entrada), 0, -1):
-    if entrada[j-1] != ' ':
-        print(f'{entrada[0:j].upper()}{entrada[j:].lower()}')
-print(entrada.lower())
+    for j in range(len(entrada), 0, -1):
+        if entrada[j-1] != ' ':
+            print(f'{entrada[0:j].upper()}{entrada[j:].lower()}')
+    print(entrada.lower())
