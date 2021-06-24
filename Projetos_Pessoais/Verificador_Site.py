c9c0ba86974985a6339d242bd38c92ad2dfc4698
Mateus <matebatichotti@gmail.com>
2021-06-24 16:42:18 -0300

    Update Verificador_Site.py

--- Projetos_Pessoais/Verificador_Site.py
+++ Projetos_Pessoais/Verificador_Site.py
@@ -1,2 +1,22 @@
-import socket
+import urllib.error
+import urllib.request
+from time import sleep
 
+# Verifica a cada 10 segundos se o site em questão está disponivel ou não
+
+print('\033[1;35m VERIFICADOR DE ATIVIDADE 1.0\033[m')
+print('   >>Digite \033[1:31m\exit\033[m para sair \n')
+
+entrada = input('\033[34mTarget site: \033[m') #Link do site
+
+while True:
+    try:
+        site = urllib.request.urlopen(entrada)
+    except urllib.error.URLError:
+        print('\033[31mSITE INDISPONIVEL\033[m')
+        sleep(10)
+    else:
+        print(f'\033[32mSITE DISPONIVEL\033[m')
+        break
+
+print('\033[1:31mFim da execução do programa\033[m')
