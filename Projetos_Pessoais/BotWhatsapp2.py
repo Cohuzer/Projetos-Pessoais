2545651fd91a07c9120767d231753dc9218b4f85
Mateus <matebatichotti@gmail.com>
2021-09-17 14:15:12 -0300

    Update BotWhatsapp2.py

--- Projetos_Pessoais/BotWhatsapp2.py
+++ Projetos_Pessoais/BotWhatsapp2.py
@@ -28,6 +28,39 @@ def lerZeroUm(mensagem):
     return retorno
 
 
+def inicializadorWebdriver():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    try:
+        site = urllib.request.urlopen('https://web.whatsapp.com/')
+    except urllib.error.URLError:
+        print('\033[31mWHATSAPP INDISPONIVEL\033[m')
+    else:
+        driver.get('https://web.whatsapp.com/')
+        time.sleep(20)
+
+
+def buscar_contato(contato):
+        campo_pesquisa = driver.find_element_by_xpath('//div[contains(@class, "copyable-text selectable-text")]')
+        #Ambos os Xpath's servem para encontrar as caixas de busca e mensagem do whatsapp
+        time.sleep(3)
+        campo_pesquisa.click()
+        campo_pesquisa.send_keys(contato)
+        campo_pesquisa.send_keys(Keys.ENTER)
+
+
+def enviar_mensagem(mensagem):
+    campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class, "copyable-text selectable-text")]')
+    #campo_mensagem[0] == Buscar contatos; campo_mensagem[1] == escrever a mensagem
+    campo_mensagem[1].click()
+    time.sleep(1)
+    campo_mensagem[1].send_keys(mensagem)
+    campo_mensagem[1].send_keys(Keys.ENTER)
+
+
+def enviar_figurinha():
+    pass
+
+
 print('='*60)
 print(' '*20, 'WHATSAPP BOT 2')
 print('='*60)
@@ -37,3 +70,5 @@ mensagem = str
 turner_sticker = lerZeroUm("\nDigite para:\n0-Mensagem de Texto\n1-Figurinha\n>> ")
 contatos = lerList("Insira o nome exato do contato ou grupo desejado")
 quantidade_mensagens = int(len(contatos))
+
+inicializadorWebdriver()
